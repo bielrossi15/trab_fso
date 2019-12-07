6de49690b3c57d8c49b10c356dde170b026bf02b
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-12-07 16:23:30 -0300

    Teste B

--- trabfinal.c
+++ trabfinal.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
-#include<sys/stat.h>
-#include<dirent.h>
 #include<string.h>
 #include<stdlib.h>
-#include<bzlib.h>
-#include <pthread.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<pthread.h>
 
 
-struct FILE {
+struct FILE_P {
     char orig_path[PATH_MAX];
     char dest_path[PATH_MAX];
-    struct FILE *next;
-}; typedef struct FILE File;
+    struct FILE_P *next;
+}; typedef struct FILE_P File_P;
+
+struct FILE_I {
+    char orig_path[PATH_MAX];
+    char dest_path[PATH_MAX];
+    struct FILE_I *next;
+}; typedef struct FILE_I File_I;
 
 struct FILES {
-    int number_files;
-    File *file;
+    int number_files_p;
+    int number_files_i;
+    File_P *file_p;
+    File_I *file_i;
 }; typedef struct FILES Files;
 
 
@@ -32,7 +37,7 @@ void pool_of_producers(char *path, char *d_path, Files *files){
         while (n--) {
             if (strcmp(dir_arq_name_list[n]->d_name, ".")!=0 && strcmp(dir_arq_name_list[n]->d_name, "..")!=0){
                 if (dir_arq_name_list[n]->d_type == DT_DIR){
-                    
+
                     char subdir_path[PATH_MAX], subdir_d_path[PATH_MAX];
                     strcpy(subdir_path, path);
                     strcat(subdir_path, "/");
@@ -42,7 +47,7 @@ void pool_of_producers(char *path, char *d_path, Files *files){
                     strcat(subdir_d_path, "/");
                     strcat(subdir_d_path, dir_arq_name_list[n]->d_name);
                     mkdir(subdir_d_path, 0700);
-
+                    
                     pool_of_producers(subdir_path, subdir_d_path, files);
                 } else {
                     char orig_path[PATH_MAX], dest_path[PATH_MAX];
@@ -56,12 +61,21 @@ void pool_of_producers(char *path, char *d_path, Files *files){
                     strcat(dest_path, dir_arq_name_list[n]->d_name);
                     strcat(dest_path, ".bz2");
 
-                    File *file = (File *)malloc(sizeof(File));
-                    strcpy(file->orig_path, orig_path);
-                    strcpy(file->dest_path, dest_path);
-                    file->next = files->file;
-                    files->file = file;
-                    files->number_files++;
+                    if (n%2 == 0){
+                        File_P *file_p = (File_P *)malloc(sizeof(File_P));
+                        strcpy(file_p->orig_path, orig_path);
+                        strcpy(file_p->dest_path, dest_path);
+                        file_p->next = files->file_p;
+                        files->file_p = file_p;
+                        files->number_files_p++;
+                    } else {
+                        File_I *file_i = (File_I *)malloc(sizeof(File_I));
+                        strcpy(file_i->orig_path, orig_path);
+                        strcpy(file_i->dest_path, dest_path);
+                        file_i->next = files->file_i;
+                        files->file_i = file_i;
+                        files->number_files_i++;
+                    }
                 }
             }
             free(dir_arq_name_list[n]);
@@ -70,33 +84,59 @@ void pool_of_producers(char *path, char *d_path, Files *files){
     }
 }
 
-void pool_of_consumers(Files *files){
-    while(files->file!=NULL && files->number_files!=0){
-        File *file;
-        file = files->file;
+void *pool_of_consumers_p(void *f){
+    Files *files = (Files *)f;
+    while(files->file_p!=NULL && files->number_files_p!=0){
+        File_P *file_p;
+        file_p = files->file_p;
 
         char command[PATH_MAX+10];
         strcpy(command, "bzip2 -c ");
-        strcat(command, file->orig_path);
+        strcat(command, file_p->orig_path);
         strcat(command, " > ");
-        strcat(command, file->dest_path);
+        strcat(command, file_p->dest_path);
         popen(command, "r");
         wait(NULL);
 
-        files->file = files->file->next;
-        files->number_files--;
+        files->file_p = files->file_p->next; 
+        files->number_files_p--;
     }
+    pthread_exit(0);
+}
+
+void *pool_of_consumers_i(void *f){
+    Files *files = (Files *)f;
+    while(files->file_i!=NULL && files->number_files_i!=0){
+        File_I *file_i;
+        file_i = files->file_i;
+
+        char command[PATH_MAX+10];
+        strcpy(command, "bzip2 -c ");
+        strcat(command, file_i->orig_path);
+        strcat(command, " > ");
+        strcat(command, file_i->dest_path);
+        popen(command, "r");
+        wait(NULL);
+
+        files->file_i = files->file_i->next; 
+        files->number_files_i--;
+    }
+    pthread_exit(0);
 }
 
 int main(int argc, char *argv[]){
     
+    pthread_t thread_1, thread_2;
+
     char orig_dir[PATH_MAX], temp_dest_dir[PATH_MAX], dest_dir[PATH_MAX], *path_part;
     strcpy(orig_dir, argv[1]);
     strcpy(dest_dir, argv[2]);
 
     Files *files = (Files *)malloc(sizeof(Files));
-    files->number_files = 0;
-    files->file = NULL;
+    files->file_p = NULL;
+    files->file_i = NULL;
+    files->number_files_p = 0;
+    files->number_files_i = 0;
 
     char *temp = strdup(orig_dir);
     strcpy(temp_dest_dir, "");
@@ -112,7 +152,12 @@ int main(int argc, char *argv[]){
     mkdir(temp_dest_dir, 0700);
 
     pool_of_producers(orig_dir, temp_dest_dir, files);
-    pool_of_consumers(files);
+
+    pthread_create(&thread_1, NULL, pool_of_consumers_p, files);
+    pthread_create(&thread_2, NULL, pool_of_consumers_i, files);
+
+    pthread_join(thread_1, NULL);
+    pthread_join(thread_2, NULL);
 
     char command[2*PATH_MAX + 30];
     strcpy(command, "tar -cf ");
@@ -123,6 +168,6 @@ int main(int argc, char *argv[]){
     strcat(command, " --remove-files");
     popen(command, "r");
     wait(NULL);
-    
+
     return 0;
 }
